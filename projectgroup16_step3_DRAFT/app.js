db11453f3f1aa99d5b06ae5abb3efe83211b7996
Lon <dannalo@oregonstate.edu>
2026-05-06 22:12:19 -0700

    Update citations and reset patients page baseline

--- projectgroup16_step3_DRAFT/app.js
+++ projectgroup16_step3_DRAFT/app.js
@@ -10,6 +10,8 @@ const { engine } = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 9116;
 
+const db = require('./database/db-connector');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
