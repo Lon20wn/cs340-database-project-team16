205dac31bea53a8477b0edbfe7ec2a60f6425bc7
Shani PlunkettdelaCruz <plunkesh@classwork.engr.oregonstate.edu>
2026-05-06 17:55:44 -0700

    Add db_connector file

--- projectgroup16_step3_DRAFT/database/db-connector.js
+++ projectgroup16_step3_DRAFT/database/db-connector.js
@@ -1,4 +1,25 @@
 // Step 3 draft note:
 // This file is intentionally a placeholder. Database wiring is completed in later steps.
 
-module.exports = null;
+// Citation for the following code:
+// Date: 4/1/2026
+// Copied from CS340 Activity2 starter code.
+// Source URL: https://canvas.oregonstate.edu/courses/2042369/assignments/10464646?module_item_id=26640125
+
+
+
+// Get an instance of mysql we can use in the app
+let mysql = require('mysql2')
+
+// Creae a 'connection pool' using the provided credentials
+const pool = mysql.createPool({
+    waitForConnections: true,
+    connectionLimit: 10,
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 'cs340_plunkesh',
+    password: 'TBc92DLG7NDK',
+    database: 'cs340_plunkesh'
+}).promise(); // This makes it so we can use async/await rather than callbacks
+
+//Export it for use in our application
+module.exports = pool;
